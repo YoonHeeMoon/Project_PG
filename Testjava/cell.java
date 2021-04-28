d3c24afe67fd6ff2f3fae9cfd61943b0edaf6e59
YoonHeeMoon <yhm721@naver.com>
2021-04-28 21:57:11 +0900

    Make cell.java

--- Testjava/cell.java
+++ Testjava/cell.java
@@ -1,28 +1,26 @@
-public class cell { 
-    public static void main(String[] args) { 
-       int N = 10;
-       int CELLS = 2 * N;
- 
-       boolean[] cells = new boolean[CELLS];      // cellular automaton at time t
-       boolean[] old   = new boolean[CELLS];      // cellular automaton at time t-1
-       cells[CELLS/2] = true;
-   
-       for (int t = 1; t < N; t++) {
- 
-          // draw current row
-          for (int i = 0; i < CELLS; i++) {
-             if(cells[i]) System.out.print("*");
-             else         System.out.print(" ");
-          }
-          System.out.println("");
- 
-          // save current row
-          for (int i = 0; i < CELLS; i++)
-             old[i] = cells[i];
- 
-          // update cells according to rule 90
-          for (int i = 1; i < CELLS - 1; i++)
-             cells[i] = old[i-1] ^ old[i+1];      // ^ means XOR
-       }
-    }
- }
+public class cell {
+   static int[][] cell(int[][] map) {
+      int n = map.length;
+      int m = map[0].length;
+      int d[][] = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 0 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+      int[][] tmp = new int[n][m];
+      for (int i = 0; i < map.length; i++) {
+         for (int j = 0; j < map[i].length; j++) {
+            int st = 0;
+            for (int k = 0; k < 9; k++) {
+               if (i + d[k][0] < n && i + d[k][0] >= 0 && j + d[k][1] < m && j + d[k][1] >= 0) {
+                  if (map[i + d[k][0]][j + d[k][1]] == 0) {
+                     st++;
+                  }
+               }
+            }
+            if (st < 5) {
+               tmp[i][j] = 1;
+            } else {
+               tmp[i][j] = 0;
+            }
+         }
+      }
+      return tmp;
+   }
+}
--- Testjava/test.java
+++ Testjava/test.java
@@ -1,33 +1,46 @@
 import java.util.Random;
 
-// cellullar Automatic
-public class test{
+public class test {
     public static void main(String[] args) {
         int[][] map = new int[10][100];
         Random ran = new Random();
-        
-        for(int i = 0 ; i < 10 ; i ++){
-            for(int j = 0 ; j < 100 ; j++){
-                map[i][j]=ran.nextInt(2);
-                System.out.print(map[i][j]==0?" ":"#");
+        cell c = new cell();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                map[i][j] = ran.nextInt(100);
+                if(map[i][j]>45){
+                    map[i][j]=0;
+                }else{
+                    map[i][j]=1;
+                }
+                System.out.print(map[i][j] == 0 ? " " : "#");
+            }
+            System.out.println();
+        }
+        System.out.println("#Gen1");
+        map = c.cell(map);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.print(map[i][j] == 0 ? " " : "#");
+            }
+            System.out.println();
+        }
+        System.out.println("#Gen2");
+        map = c.cell(map);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.print(map[i][j] == 0 ? " " : "#");
+            }
+            System.out.println();
+        }
+        System.out.println("#Gen3");
+        map = c.cell(map);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.print(map[i][j] == 0 ? " " : "#");
             }
             System.out.println();
         }
     }
-    static int[][] cell(int[][] map){
-        int n = map.length;
-        int m = map[0].length;
-        int d[][] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
-        int[][] tmp = new int[n][m];
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                    int st = 0;
-                    if(i-1)
-                }
-            }
-        }
-        return map;
-
-    }
 }
-
